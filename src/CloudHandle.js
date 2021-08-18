5effb59358b7f774af2b2eb7f9fbe4372c4dfb2c
Null <57363125+SocketDisconnect@users.noreply.github.com>
2021-08-18 16:47:00 +0800

    Add files via upload

--- src/CloudHandle.js
+++ src/CloudHandle.js
@@ -1,9 +1,14 @@
 const CreateBrowser = require('./CreateSession');
-//const { Cookie } = require('tough-cookie');
 
-async function HandleChallenge(url) {
-  const browser = await CreateBrowser();
+async function HandleChallenge(url,proxy) {
+  const browser = await CreateBrowser(proxy);
   const page = await browser.newPage();
+
+  if (proxy){
+    let user = proxy.split('@')[0].split(':')[0];
+    let passw = proxy.split('@')[0].split(':')[1];
+    await page.authenticate({username:user,password:passw});
+  }
 
   try {
     await page.goto(url, {
@@ -14,25 +19,29 @@ async function HandleChallenge(url) {
     let count = 1;
     let content = await page.content();
     
-
     while(content.includes('challenge-form')) {
       await page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: 15*1000 });
       content = await page.content();
       if (count++ === 25) {
-        throw new Error('timeout on just a moment');
+        throw new Error('timeout');
       }
+    }
+    
+    if (content.includes('cf-error-details') || content.includes('cf_captcha_kind')){
+      throw new Error('proxy ban');
     }
     
     let html = await page.content();
     let cookie = await page.cookies();
     await browser.close();
-    
+      
     return {
-      code:200,
-      data:html,
-      cookie:cookie
+        code:200,
+        data:html,
+        cookie:cookie
     }
-  } catch (e) {
+  } 
+  catch (e) {
     await browser.close();
     return {
       code: 500,
--- src/CreateSession.js
+++ src/CreateSession.js
@@ -2,10 +2,17 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
-async function CreateBrowser(){
+async function CreateBrowser(proxy){
+    let args = ['--no-sandbox', '--disable-setuid-sandbox'];
+    
+    if (proxy){
+        proxy = proxy.split('@')[1];
+        args.push(`--proxy-server=${proxy}`);
+    }
+    
     return await puppeteer.launch({
         headless:false,
-        args:['--no-sandbox', '--disable-setuid-sandbox']
+        args:args
     });
 }
 
